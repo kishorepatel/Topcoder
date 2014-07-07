5c3271fd3ad70d22eb100ee023858f0e7f2aa35a
kishorepatel <kishore.patel.india@gmail.com>
2014-07-07 22:53:20 +0530

    updates

---
+++ src/HackerRank/Arrays_and_strings/CountingSort1/Solution.java
@@ -0,0 +1,87 @@
+
+package Arrays_and_strings.CountingSort1;
+import java.util.*;
+import java.io.*;
+
+public class Solution implements Runnable {
+		
+	public void solve() throws IOException {
+		int N = nextInt();
+                int[] cnt = new int[100];
+                for(int i = 0; i < N; i++) cnt[nextInt()]++;
+                
+                for(int i = 0; i < cnt.length; i++) System.out.print(cnt[i] + " ");
+                System.out.println();
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//-----------------------------------------------------------
+	public static void main(String[] args) {
+		new Solution().run();
+	}
+
+        public void print1Int(int[] a){
+                for(int i = 0; i < a.length; i++)
+                        System.out.print(a[i] + " ");
+                System.out.println();
+        }
+        
+        public void print2Int(int[][] a){
+                for(int i = 0; i < a.length; i++){
+                        for(int j = 0; j < a[0].length; j++){
+                                System.out.print(a[i][j] + " ");
+                        }
+                        System.out.println();
+                }
+        }
+        
+	public void run() {
+		try {
+			in = new BufferedReader(new InputStreamReader(System.in));
+			tok = null;
+			solve();
+			in.close();
+		} catch (IOException e) {
+			System.exit(0);
+		}
+	}
+
+	public String nextToken() throws IOException {
+		while (tok == null || !tok.hasMoreTokens()) {
+			tok = new StringTokenizer(in.readLine());
+		}
+		return tok.nextToken();
+	}
+
+	public int nextInt() throws IOException {
+		return Integer.parseInt(nextToken());
+	}
+
+	public long nextLong() throws IOException {
+		return Long.parseLong(nextToken());
+	}
+
+	public double nextDouble() throws IOException {
+		return Double.parseDouble(nextToken());
+	}
+
+	BufferedReader in;
+	StringTokenizer tok;
+}
